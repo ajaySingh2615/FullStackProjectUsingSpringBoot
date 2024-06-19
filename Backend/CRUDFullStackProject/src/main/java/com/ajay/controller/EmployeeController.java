76450ca9a0bcc2f693fcdedbefefe35c925effbf
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-06-19 23:42:17 +0530

    "Add Employee component done"

--- Backend/CRUDFullStackProject/src/main/java/com/ajay/controller/EmployeeController.java
+++ Backend/CRUDFullStackProject/src/main/java/com/ajay/controller/EmployeeController.java
@@ -1,14 +1,25 @@
 package com.ajay.controller;
 
-import com.ajay.dto.EmployeeDto;
-import com.ajay.service.EmployeeService;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ajay.dto.EmployeeDto;
+import com.ajay.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
---
+++ Frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <span>All right reversed 2024 by cadt</span>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
